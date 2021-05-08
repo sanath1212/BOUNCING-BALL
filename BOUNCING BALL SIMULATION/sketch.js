5764c8cf8c3796b32ffa1604d37c1c342b0f10a4
sanath1212 <67041029+sanath1212@users.noreply.github.com>
2021-05-08 11:27:38 -0700

    natural wind added

--- BOUNCING BALL SIMULATION/sketch.js
+++ BOUNCING BALL SIMULATION/sketch.js
@@ -1,110 +1,137 @@
-let x;
-let y;
-let size = 20;
-let gravity = 1;
-let time = 0;
-let speed = 0;
-let mass = 0.1;
-let wind_speed = 5;
-let drag = 100;
-let userwind = 0.4;
-let friction = 0.06;
+var x;
+var y;
+var time;
+var weight;
+var gravity;
+var mass;
+var speed;
+var wind;
+var pointx;
+var pointy;
+var xacc;
+var x_final_velocity;
+var friction;
+var stop_wind;
+var radius;
+
+class entity {
+  constructor() {
+    this.radius = 20;
+    this.x = Math.floor((Math.random() * (width - ((this.radius / 2) + 10))) + 1);
+  	this.y = Math.floor((Math.random() * height / 9) + 1);
+    this.time = 0;
+    this.weight;
+    this.gravity = 1;
+    this.mass = 0.1;
+    this.speed;
+    this.wind = 0;
+    this.pointx;
+    this.pointy;
+    this.xacc = 0;
+    this.x_final_velocity = 0;
+    this.friction = 0.001;
+    this.stop_wind = 'START';
+  }
+
+    stop_wind_value() {
+    	this.stop_wind = document.getElementById('stop_wind_value_value').value;
+    }
+
+		massvalue() {
+			this.mass = document.getElementById('mass_value').value;
+		}
+
+		gravityvalue() {
+			this.gravity = document.getElementById('gravity_value').value;
+		}
+
+		sizevalue() {
+			this.size = document.getElementById('size_value').value;
+		}
+
+    gravity_physics() {
+    	this.time++;
+    	this.weight = this.gravity * this.mass;
+      this.speed = this.weight * this.time;
+    	this.y += this.speed;
+    }
+
+    wind_physics() {
+    	if (this.stop_wind != 'STOP') {
+    		this.pointx = random(width);
+    		this.pointy = random(height);
+    		if (this.pointx > this.pointy) {
+    			this.wind += 0.001;
+    		} else {
+    			this.wind -= 0.001;
+    		}
+    	} else {
+    		this.wind = 0;
+    	}
+    	this.x_final_velocity += this.wind;
+    	this.x += this.x_final_velocity;
+    }
+
+    friction_physics() {
+    	if (this.y > ((height - (this.radius / 2)) - 1.5)) {
+    	    if (this.x_final_velocity < 0) {
+    	      this.x_final_velocity += this.friction;
+    	    }
+    	    if (this.x_final_velocity > 0) {
+    	      this.x_final_velocity -= this.friction;
+    	    }
+    	}
+    }
+
+    // collision_physics() {
+    //
+    // }
+
+    edge() {
+    	if (this.y > (height - ((this.radius / 2) + 1))) {
+    		    this.time = this.time * -1;
+    		    this.speed = this.weight * this.time;
+    		    this.time -= this.speed;
+    		    this.y += this.speed;
+    	}
+
+    	if (this.x > (width - (this.radius / 2))) {
+    		this.x_final_velocity *= -1;
+    	}
+
+    	if (this.x <(this.radius / 2)) {
+    		this.x_final_velocity *= -1;
+    	}
+    }
+
+    display() {
+      stroke(255, 0, 0, (255 / 1.5));
+    	fill(255, 0, 0, (255 / 1.5));
+    	ellipse(this.x, this.y, this.radius, this.radius);
+    }
+}
+var no_of_entity = 100;
+let new_entity = [];
 
 function setup() {
-	createCanvas(800, 500);
-	x = (width / 2);
-	y = (size / 2);
+	createCanvas(500, 500);
+  for (var i = 0; i < no_of_entity; i++) {
+    new_entity[i] = new entity();
+  }
 }
 
 function draw() {
-	print(wind_speed);
-	print(y);
-	background(240);
-
-	gravity_physics();
-	friction_physics();
-	if (mouseIsPressed) {
-		wind_physics_user();
-	}
-	wind_physics();	
-	drag_physics();	
-
-	stroke(255, 0, 0, (255 / 1.5));
-	fill(255, 0, 0, (255 / 1.5));
-	ellipse(x, y, size, size);
-}
-
-function massvalue() {
-	mass = document.getElementById('mass_value').value;
-}
-
-function gravityvalue() {
-	gravity = document.getElementById('gravity_value').value;
-}
-
-function sizevalue() {
-	size = document.getElementById('size_value').value;
-}
-
-function gravity_physics() {
-	time++;
-	weight = gravity * mass;
-    speed = weight * time;
-    y += speed;
-    edge();
-}
-
-function friction_physics() {
-	if (y > ((height - (size / 2)) - 1.5)) {
-	    if (wind_speed < 0) {
-	      wind_speed += friction;
-	    }
-	    if (wind_speed > 0) {
-	      wind_speed -= friction;
-	    }
-	}
-}
-
-function wind_physics() {
-	x += wind_speed;
-	edge();
-}
-
-function wind_physics_user() {
-	wind_speed += -userwind;
-	x += wind_speed;
-	edge();
-}
-
-function drag_physics() {
-	speed -= drag;
-	time++;
-	weight = gravity * mass;
-	speed = weight * time;
-	edge();
-}
-
-function edge() {
-	if (y > (height - ((size / 2) + 1))) {
-		    time = time * -1;
-		    speed = weight * time;
-		    time -= speed;
-		    y += speed;
-		}
-
-		if (x > (width - ((size / 2) + 1))) {
-			time = time * -1;
-			wind_speed *= -1;
-			time = time * -1;
-		    speed = weight * time;
-		    time -= speed;
-		}
-
-		if (x < ((size / 2) + 1)) {
-			time = time * -1;
-			wind_speed *= -1;
-			time = time * -1;
-		    speed = weight * time;
-		    time -= speed;
-		}
+  background(240);
+  for (var i = 0; i < no_of_entity; i++) {
+    new_entity[i].gravity_physics();
+    new_entity[i].wind_physics();
+    new_entity[i].friction_physics();
+		// new_entity[i].collision_physics();
+    new_entity[i].edge();
+    new_entity[i].display();
+  }
+  stop_wind_value();
+  massvalue();
+  gravityvalue();
+  sizevalue();
 }
